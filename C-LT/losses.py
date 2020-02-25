220306f35c359788080325d7a5b092d3fee10778
Abdullah Jamal <abdullahjml11@gmail.com>
2020-02-24 19:17:45 -0500

    Update losses.py

--- C-LT/losses.py
+++ C-LT/losses.py
@@ -1,1 +1,31 @@
+import math
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
+# copied from https://github.com/kaidic/LDAM-DRW/blob/master/losses.py
+class LDAMLoss(nn.Module):
+    
+    def __init__(self, cls_num_list, max_m=0.5, weight=None, s=30, reduce_=False):
+        super(LDAMLoss, self).__init__()
+        m_list = 1.0 / np.sqrt(np.sqrt(cls_num_list))
+        m_list = m_list * (max_m / np.max(m_list))
+        m_list = torch.cuda.FloatTensor(m_list)
+        self.m_list = m_list
+        assert s > 0
+        self.s = s
+        self.weight = weight
+        self.reduce = reduce_
+
+    def forward(self, x, target):
+        index = torch.zeros_like(x, dtype=torch.uint8)
+        index.scatter_(1, target.data.view(-1, 1), 1)
+        
+        index_float = index.type(torch.cuda.FloatTensor)
+        batch_m = torch.matmul(self.m_list[None, :], index_float.transpose(0,1))
+        batch_m = batch_m.view((-1, 1))
+        x_m = x - batch_m
+    
+        output = torch.where(index, x_m, x)
+        return F.cross_entropy(self.s*output, target, reduce=self.reduce)
